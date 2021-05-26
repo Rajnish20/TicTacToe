5c6405da464605cb832ff6c0bdf2248b36d65e3a
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-26 18:59:53 +0530

    Board created to play

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class TicTacToeGame {
+    public char[] createBoard() {
+        char[] board = new char[9];
+        Arrays.fill(board, ' ');
+        return board;
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to TicTacToe");
+        TicTacToeGame ticTacToe = new TicTacToeGame();
+        char[] board = ticTacToe.createBoard();
     }
 }
